eb0e74f0e907c761ce76dda7631d4da2627f519b
Simba Wangu <simbawangulu@gmail.com>
2022-12-21 17:04:10 +0300

    Revert "Header"

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React, { ReactElement } from 'react'
-import { Header } from './container'
 import { Navbar } from './components'
-import './App.scss'
 
 const App = (): ReactElement => {
   return (
     <div className="App">
       <Navbar />
-      <Header />
     </div>
   )
 }
