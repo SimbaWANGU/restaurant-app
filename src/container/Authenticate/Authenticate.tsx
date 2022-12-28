08640deb359a3f7f5e26670c4cd05fd49962154b
SimbaWANGU <simbawangulu@gmail.com>
2022-12-28 21:02:16 +0300

    make minor change

--- src/container/Authenticate/Authenticate.tsx
+++ src/container/Authenticate/Authenticate.tsx
@@ -3,8 +3,6 @@ import React, { ReactElement, useState } from 'react'
 const Authenticate = (): ReactElement => {
   const [value, setValue] = useState(0)
 
-  setValue(24)
-
   return (
     <div className='authenticate-modal'>
       {value}
