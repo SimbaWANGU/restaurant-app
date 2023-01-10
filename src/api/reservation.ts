83b0e477e7069b951a97ec07b04b0768e2554eaa
Simba Wangu <simbawangulu@gmail.com>
2023-01-10 22:49:37 +0300

    rewrite fetch methods with async await syntax

--- src/api/reservation.ts
+++ src/api/reservation.ts
@@ -1,38 +1,47 @@
-const makeReservation = async (username: string, date: string, time: string, guests: number, toast: any): Promise<void> => {
-  await fetch('https://restaurant-server-twu5.onrender.com/reserve/create', {
+const makeReservation = async (username: string, date: string, time: string, guests: number, toast: any): Promise<any> => {
+  const response = await fetch('https://restaurant-server-twu5.onrender.com/reserve/create', {
     method: 'post',
     headers: {
       'Content-type': 'application/x-www-form-urlencoded'
     },
     body: `username=${username}&date=${date}&time=${time}&guests=${guests}`
   })
-    .then(async (res) => {
-      const data = await res.json()
+  let data
+  switch (response.status) {
+    case 200:
+      data = await response.json()
       if (data.success === 'Resevation has been created') {
         toast.success(data.success)
       } else if (data.error === 'An error occurred' || data.error === 'Incomplete Reservation') {
         toast.error(data.error)
       }
-    })
-    .catch((err) => {
-      console.log(err.message)
-      toast.error('An error occurred')
-    })
+      break
+    case 500:
+      return 'Error'
+  }
 }
 
-const getReservation = async (callback: any): Promise<void> => {
+const getReservation = async (): Promise<any> => {
   const user = JSON.parse(localStorage.getItem('gericht-user') as string)
-  await fetch(`https://restaurant-server-twu5.onrender.com/reserve/${user.username as string}`, {
+  const response = await fetch(`https://restaurant-server-twu5.onrender.com/reserve/${user.username as string}`, {
     method: 'get',
     headers: {
       'Content-type': 'application/x-www-form-urlencoded'
     }
   })
-    .then(async (res) => {
-      const data = await res.json()
-      callback(data.myReservations)
-    })
-    .catch(() => {})
+  let data
+  switch (response.status) {
+    case 200:
+      data = await response.json()
+      return data.myReservations
+      // if (data.success === 'Resevation has been created') {
+      //   toast.success(data.success)
+      // } else if (data.error === 'An error occurred' || data.error === 'Incomplete Reservation') {
+      //   toast.error(data.error)
+      // }
+    case 500:
+      return 'Error'
+  }
 }
 
 export { makeReservation, getReservation }
