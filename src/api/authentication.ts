3d5e341354a0b078f8d5a63ea12a7a0a029560e5
Simba Wangu <simbawangulu@gmail.com>
2023-01-10 22:49:16 +0300

    rewrite fetch methods with async await syntax

--- src/api/authentication.ts
+++ src/api/authentication.ts
@@ -1,40 +1,39 @@
-const register = async (username: string, email: string, password: string): Promise<void> => {
-  await fetch('https://restaurant-server-twu5.onrender.com/auth/register', {
+const register = async (username: string, email: string, password: string): Promise<any> => {
+  const response = await fetch('https://restaurant-server-twu5.onrender.com/auth/register', {
     method: 'post',
     headers: {
       'Content-type': 'application/x-www-form-urlencoded'
     },
     body: `username=${username}&email=${email}&password=${password}`
   })
-    .then(async (res) => {
-      const data = await res.json()
+  let data
+  switch (response.status) {
+    case 200:
+      data = await response.json()
       localStorage.setItem('gericht-user', JSON.stringify(data))
-    })
-    .catch((err: Error) => {
-      console.log(err)
-      return {
-        error: err.message
-      }
-    })
+      return data.message
+    case 500:
+      return 'Error'
+  }
 }
 
-const login = async (username: string, password: string): Promise<void> => {
-  await fetch('https://restaurant-server-twu5.onrender.com/auth/login', {
+const login = async (username: string, password: string): Promise<any> => {
+  const response = await fetch('https://restaurant-server-twu5.onrender.com/auth/login', {
     method: 'post',
     headers: {
       'Content-type': 'application/x-www-form-urlencoded'
     },
     body: `username=${username}&password=${password}`
   })
-    .then(async (res) => {
-      const data = await res.json()
+  let data
+  switch (response.status) {
+    case 200:
+      data = await response.json()
       localStorage.setItem('gericht-user', JSON.stringify(data))
-    })
-    .catch((err: Error) => {
-      return {
-        error: err.message
-      }
-    })
+      return data.message
+    case 500:
+      return 'Error'
+  }
 }
 
 export { register, login }
