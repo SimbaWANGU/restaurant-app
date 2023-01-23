44920af34369878b9e1dab5a7cf82192cbb73ece
Simba Wangu <simbawangulu@gmail.com>
2023-01-23 16:04:39 +0300

    display feedback from db

--- src/container/Feedback/Feedback.tsx
+++ src/container/Feedback/Feedback.tsx
@@ -1,20 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Autoplay } from 'swiper'
 import 'swiper/swiper-bundle.css'
 import './Feedback.scss'
+import { useQuery } from 'react-query'
+import { getFeedback } from '../../api/feedback'
+import { toast, ToastContainer } from 'react-toastify'
 
 interface FeedbackProps {
   handleOpenFeedbackModal: any
 }
 
+interface Feedbacks {
+  _id: string
+  username: string
+  emoji: string
+  feedback: string
+}
+
 const Feedback: React.FC<FeedbackProps> = ({ handleOpenFeedbackModal }) => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  const [slidesPerView, setSlidesPerView] = useState(3)
+
+  useEffect(() => {
+    const handleResize = (): void => {
+      setScreenWidth(window.innerWidth)
+    }
+    window.addEventListener('resize', handleResize)
+    screenWidth <= 650
+      ? (setSlidesPerView(2))
+      : (setSlidesPerView(3))
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [screenWidth])
+
+  const { data, status } = useQuery('feedbacks', async () => {
+    return await getFeedback()
+  })
+
+  if (status === 'loading') {
+    return (
+      <div className='empty-reservations'>
+        <h1>
+          Please wait...
+        </h1>
+      </div>
+    )
+  }
+
+  if (status === 'error') {
+    toast.error('An error occurred. Please try again...', {
+      position: 'top-center',
+      autoClose: false,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      theme: 'light'
+    })
+
+    return (
+      <div className='empty-reservations'>
+        <h1>
+          Cannot retrieve your reservations at this time
+        </h1>
+        <ToastContainer />
+      </div>
+    )
+  }
+
   return (
     <div className='feedback-container'>
       <h2>Reviews</h2>
       <Swiper
         spaceBetween={50}
-        slidesPerView={3}
+        slidesPerView={slidesPerView}
         loop={true}
         autoplay={{
           delay: 2500,
@@ -22,39 +83,27 @@ const Feedback: React.FC<FeedbackProps> = ({ handleOpenFeedbackModal }) => {
         }}
         modules={[Autoplay]}
       >
+        {data.length === 0
+          ? (
+            <></>
+            )
+          : (
+              data.map((item: Feedbacks) => (
+              <SwiperSlide key={item._id}>
+                <div className='slide-container' data-aos="flip-up">
+                  <p>{item.emoji}</p>
+                  <p>{item.feedback}</p>
+                  <p>{item.username}</p>
+                </div>
+              </SwiperSlide>
+              ))
+            )
+        }
         <SwiperSlide>
           <div className='slide-container' data-aos="flip-up">
             <p>😀</p>
             <p>I loved their service, they were very considerate and their food is amazing</p>
-            <p>by <span>Simberella</span></p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='slide-container' data-aos="flip-up">
-            <p>😀</p>
-            <p>I loved their service, they were very considerate and their food is amazing</p>
-            <p>by <span>Simberella</span></p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='slide-container' data-aos="flip-up">
-            <p>😀</p>
-            <p>I loved their service, they were very considerate and their food is amazing</p>
-            <p>by <span>Simberella</span></p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='slide-container' data-aos="flip-up">
-            <p>😀</p>
-            <p>I loved their service, they were very considerate and their food is amazing</p>
-            <p>by <span>Simberella</span></p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='slide-container' data-aos="flip-up">
-            <p>😀</p>
-            <p>I loved their service, they were very considerate and their food is amazing</p>
-            <p>by <span>Simberella</span></p>
+            <p>Simberella</p>
           </div>
         </SwiperSlide>
       </Swiper>
